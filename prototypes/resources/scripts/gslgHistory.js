24e20f2ff7b7f4f394114fb8c044c2e6dc2948b8
gslg-1 <61031185+gslg-1@users.noreply.github.com>
2022-01-29 21:58:25 +0100

    First Dummy History Graph created

--- prototypes/resources/scripts/gslgHistory.js
+++ prototypes/resources/scripts/gslgHistory.js
@@ -46,20 +46,43 @@ class HistoryMarker{
     this.mContainer.appendChild(element);
   }
 }
+
 class HistoryGraph{
-  constructor(container, Dbg){
-    this.mContainer = container;
-    this.mDbg = Dbg;
-
-    this.attachMe();
-    this.mDbg.Dbg("History Graph - Created");
+  constructor(svg, array){
+    this.mSvg = svg;
+    this.fill(array);
   }
-  attachMe() {
-    this.mSVG = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-    this.mContainer.appendChild(this.mSVG);
-  }
-  configureMe() {
+  fill(array) {
+    
+    let PosStart = 32;
+    let PosDist = 160;
+    for (let i = 0 ; i < array.length; i++ )
+    {
+      let nLine = document.createElementNS("http://www.w3.org/2000/svg","line");
+      let nDot = document.createElementNS("http://www.w3.org/2000/svg","circle");
+      
+      let y1 = PosStart + (PosDist * i);
+      let y2 = PosStart + (PosDist * (i+1));
+      nLine.setAttribute('x1','50%');
+      nLine.setAttribute('y1', ''+ y1 );
+      nLine.setAttribute('x2','50%');
+      nLine.setAttribute('y2', '' + y2 );
+      nLine.setAttribute("stroke", "black");
+      nLine.setAttribute("stroke-width", "3");
+      
+      nDot.setAttribute('r','8');
+      nDot.setAttribute('cx','50%' );      
+      nDot.setAttribute('cy','' + y1);
+      nDot.setAttribute('stroke','black' );
+      nDot.setAttribute('stroke-width','2' );
+      nDot.setAttribute('fill','red' );
+      
+      console.warn('Part '+i + ' Y1 = ' + y1 + ' Y2 = ' + y2);
 
+      this.mSvg.append(nLine);
+      this.mSvg.append(nDot);
+      this.mSvg.append("New Line"+i);
+    }
   }
 }
 class History {
@@ -69,27 +92,37 @@ class History {
   }
   Create(array) {
     this.mDivMrkr = document.createElement("div");
-    this.mDivCntnt = document.createElement("div");
+    this.mDivCnt = document.createElement("div");
+    this.mSVG = document.createElementNS("http://www.w3.org/2000/svg","svg");
     
-    this.mDivMrkr.style.backgroundColor = "red";
-    this.mDivMrkr.style.width = "50%";
+    this.mDivMrkr.style.backgroundColor = "rgba(255, 0, 0, 1)"  ;
+    this.mDivMrkr.style.width = "33%";
     this.mDivMrkr.style.boxSizing = "border-box";
     this.mDivMrkr.style.cssFloat = "left";
     this.mDivMrkr.style.overflow = "auto";
     
-    this.mDivCntnt.style.backgroundColor = "blue";
-    this.mDivCntnt.style.width = "50%";
-    this.mDivCntnt.style.cssFloat = "left";
-    this.mDivCntnt.style.boxSizing = "border-box";
-    this.mDivCntnt.style.boxSizoverflowing = "auto";
+    this.mDivCnt.style.backgroundColor = "rgba(0, 255, 0, 1)";
+    this.mDivCnt.style.width = "33%";
+    this.mDivCnt.style.cssFloat = "left";
+    this.mDivCnt.style.boxSizing = "border-box";
+    this.mDivCnt.style.boxSizoverflowing = "auto";
+
+    this.mSVG.style.backgroundColor = "rgba(0, 0, 255, 1)";
+    this.mSVG.style.width = "10%";
+    this.mSVG.style.height = "6400";
+    this.mSVG.style.cssFloat = "left";
+    this.mSVG.style.boxSizing = "border-box";
+    this.mSVG.style.boxSizoverflowing = "auto";
         
     this.mContainer.style.overflow = "auto";
 
     this.mContainer.appendChild(this.mDivMrkr);
-    this.mContainer.appendChild(this.mDivCntnt);
+    this.mContainer.appendChild(this.mSVG);
+    this.mContainer.appendChild(this.mDivCnt);
 
     this.mObjMrkr = new HistoryMarker(this.mDivMrkr, array);
-    this.mObjCntnt = new HistoryContent(this.mDivCntnt, array);
+    this.mObjCntnt = new HistoryContent(this.mDivCnt, array);
+    this.mObjPath = new HistoryGraph(this.mSVG, array);
   }
   
 }
